99c0d45b5a9b129c5b5bd1fd98c33a62a83caa01
Hanie <52105795+hanikhosrav@users.noreply.github.com>
2022-02-09 03:13:37 -0800

    hash password

--- config/passport.js
+++ config/passport.js
@@ -1,11 +1,13 @@
 const adminModel = require('../model/adminModel');
 const passport = require('passport');
 const {Strategy} = require('passport-local');
+const bcrypt = require('bcrypt');
 
 passport.use(new Strategy({usernameField: 'email'}, async (email, password, done) => {
-    const user = await adminModel.findOne({email, password});
-        if(!user) return done(null, false);
-        return done(null, user);
+    const user = await adminModel.findOne({email});
+    const isMatch = await bcrypt.compare(password, user.password);
+    if(!isMatch) return done(null, false);
+    return done(null, user);
     }
 ));
 
